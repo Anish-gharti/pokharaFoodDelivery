aa44732833f6aab2a6934c6a4462f0a118d61e1c
anishchengre <anishchengre@kali.anishchengre>
2023-04-03 07:22:11 +0545

    custom image validation added in resturant profiles

--- vendor/views.py
+++ vendor/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import VendorForm, Vendor
 from accounts.forms import UserProfileForm, UserProfile
+from django.contrib import messages
 # Create your views here.
 
 
 def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
-    profile_form = UserProfileForm(instance=profile)
-    vendor_form = VendorForm(instance=vendor)
+
+    if request.method == "POST":
+        profile_form = UserProfileForm(
+            request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, "profile settings for vendor updated.")
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
     context = {
         'profile_form': profile_form,
         'vendor_form': vendor_form,
