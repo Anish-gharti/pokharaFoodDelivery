72fa3fbc78febe69a7193c9cbaf6b65eee93072d
anishchengre <anishchengre@kali.anishchengre>
2023-04-05 21:22:40 +0545

    addin test food and setup admin table for menu app

--- menu/admin.py
+++ menu/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Category, FoodItem
 # Register your models here.
 
+class Categoryadmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'vendor', 'updated_at')
+    search_fields = ('category_name', 'vendor__vendor_name__exact')
 
-admin.site.register(Category)
-admin.site.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('food_item',)}
+    list_display = ('food_item', 'category', 'vendor', 'price', 'is_available', 'updated_at')
+    search_fields = ('food_item', 'category__category_name__icontains', 'vendor__vendor_name__exact', 'is_available')
+
+
+admin.site.register(Category, Categoryadmin)
+admin.site.register(FoodItem, FoodItemAdmin)
 
