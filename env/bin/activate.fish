6fdeb2f2594d2b736974b704ce62390e6850b0f4
anishchengre <anishchengre@kali.anishchengre>
2023-04-07 22:03:10 +0545

    anish is backa

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/anishchengre/Desktop/django_projects/Pokhara_food_delivery/env"
+set -gx VIRTUAL_ENV "/home/anishchengre/Desktop/django_projects/pokharaFoodDelivery/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
