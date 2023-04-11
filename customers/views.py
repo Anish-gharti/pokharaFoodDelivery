ca0d9d56bfb9d8d4d1cb2294c67f281486234b27
anishchengre <anishchengre@kali.anishchengre>
2023-04-11 09:44:04 +0545

    cutomer profile updated

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from accounts.forms import UserInfoForm, UserProfileForm, UserProfile, User
@@ -16,12 +17,27 @@ def check_role_customer(user):
 @user_passes_test(check_role_customer)
 def customer_profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == "POST":
+        user_profile_form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        user_form = UserInfoForm(request.POST,instance=request.user)
+        if user_profile_form.is_valid() and user_form.is_valid():
+            user_profile_form.save()
+            user_form.save()
+            messages.success(request, 'userprofile updated successfully')
+            return redirect('c_profile')
+        else:
+            print(user_profile_form.errors)
+            print(user_form.errors)
 
-    user_profile_form = UserProfileForm(instance=user_profile)
-    user_form = UserInfoForm(instance=request.user)
+
+
+    else:
+        user_profile_form = UserProfileForm(instance=user_profile)
+        user_form = UserInfoForm(instance=request.user)
 
     context = {
         'profile_form': user_profile_form,
         'user_form': user_form,
+        'profile': user_profile,
     }
     return render(request, 'customers/customer_profile.html', context)
